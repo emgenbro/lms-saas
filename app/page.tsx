ea361463bb25a27fe6b59d3c56f5de3d8fe119e7
Damon Emgenbroich <emgenbro@yahoo.com>
2025-08-09 11:32:14 -0400

    implement navbar

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,7 @@ import React from 'react'
 const Page = () => {
   return (
     <div>
-      <h1 className='text-2xl underline'>Hello world!</h1>
-      <Button>Click Me</Button>
+      LMS
     </div>
   )
 }
