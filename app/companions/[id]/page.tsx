8ca956ebe3457795b9085b5b3c0fea69a0b318ed
Damon Emgenbroich <emgenbro@yahoo.com>
2025-08-09 10:29:24 -0400

    layed out application

---
+++ app/companions/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CampanionSession = () => {
+  return (
+    <div>CampanionSession
+    
+    </div>
+  )
+}
+
+export default CampanionSession
---
+++ app/companions/new/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>new campnign</div>
+  )
+}
+
+export default page
