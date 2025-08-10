95418a33a5e8e4886db4e65eac96f7e02bdb2ef8
Damon Emgenbroich <emgenbro@yahoo.com>
2025-08-10 18:52:44 -0400

    implement tiers

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,26 @@
 import CompanionCard from "@/components/CompanionCard";
-import CTA from "@/components/CTA";
 import CompanionsList from "@/components/CompanionsList";
-
-const recentSessionsCompanions = [
-  {
-    id: "321",
-    name: "Histora the Time Traveler",
-    topic: "World History",
-    subject: "history",
-    duration: 40,
-    color: "#FFD6E0",
-    bookmarked: false,
-  },
-  {
-    id: "654",
-    name: "Bio the Life Explorer",
-    topic: "Cell Biology",
-    subject: "science",
-    duration: 35,
-    color: "#D6FFD6",
-    bookmarked: false,
-  },
-];
+import CTA from "@/components/CTA";
+import {recentSessions} from "@/constants";
+import {getAllCompanions, getRecentSessions} from "@/lib/actions/companion.actions";
+import {getSubjectColor} from "@/lib/utils";
 
 const Page = async () => {
-
+    const companions = await getAllCompanions({ limit: 3 });
+    const recentSessionsCompanions = await getRecentSessions(10);
 
   return (
     <main>
       <h1>Popular Companions</h1>
 
         <section className="home-section">
-            <CompanionCard
-                id="123"
-                name="Neura the Brainy Explorer"
-                topic="Neural Network of the Brain"
-                subject="science"
-                duration={45}
-                color="#ffda6e"
-                bookmarked={true}
-            />
-            <CompanionCard
-                id="456"
-                name="Countsy the Number Wizard"
-                topic="Derivatives & Integrals"
-                subject="maths"
-                duration={30}
-                color="#e5d0ff"
-                bookmarked={true}
-            />
-            <CompanionCard
-                id="789"
-                name="Verba the Vocabulary Builder"
-                topic="language"
-                subject="English Literature"
-                duration={30}
-                color="#BDE7FF"
-                bookmarked={true}
-            />
+            {companions.map((companion) => (
+                <CompanionCard
+                    key={companion.id}
+                    {...companion}
+                    color={getSubjectColor(companion.subject)}
+                />
+            ))}
 
         </section>
 
@@ -67,7 +30,7 @@ const Page = async () => {
                 companions={recentSessionsCompanions}
                 classNames="w-2/3 max-lg:w-full"
             />
-            <CTA/>
+            <CTA />
         </section>
     </main>
   )
